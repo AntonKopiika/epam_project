03e384fc91bf04c282133dddd9d171579843852e
AntonKopiika <toxxa099@gmail.com>
2021-04-18 11:28:33 +0300

    populate employees table

--- src/service/population.py
+++ src/service/population.py
@@ -1,4 +1,7 @@
+from datetime import date
+
 from src.models.department import Department
+from src.models.employee import Employee
 from src import db
 
 
@@ -23,6 +26,37 @@ def populate_departments():
     db.session.close()
 
 
+def populate_employees():
+    employee1 = Employee(first_name="Anton", last_name="Kopiika", position="Junior Python Developer", salary=600,
+                         birthday=date(1999, 4, 2))
+    employee2 = Employee(first_name="Dmytro", last_name="Ivanov", position="Junior Python Developer", salary=600,
+                         birthday=date(2000, 1, 1))
+    employee3 = Employee(first_name="Ihor", last_name="Salo", position="Middle Python Developer", salary=1500,
+                         birthday=date(1996, 6, 6))
+    employee4 = Employee(first_name="Yurii", last_name="Morozov", position="Middle Python Developer", salary=2000,
+                         birthday=date(1997, 12, 20))
+    employee5 = Employee(first_name="Petro", last_name="Mogula", position="Senior Python Developer", salary=3000,
+                         birthday=date(1995, 9, 24))
+    employee6 = Employee(first_name="Mykola", last_name="Zerov", position="HR", salary=1000,
+                         birthday=date(1993, 8, 12))
+    employee7 = Employee(first_name="Serhiy", last_name="Burbas", position="Frontend Developer", salary=800,
+                         birthday=date(1998, 3, 7))
+
+    db.session.add(employee1)
+    db.session.add(employee2)
+    db.session.add(employee3)
+    db.session.add(employee4)
+    db.session.add(employee5)
+    db.session.add(employee6)
+    db.session.add(employee7)
+
+    db.session.commit()
+    db.session.close()
+
+
 if __name__ == '__main__':
-    populate_departments()
-    print("departments are populated!")
+    populate_employees()
+    print("employees are populated!")
+
+    # populate_departments()
+    # print("departments are populated!")
