ecb3bfa2fab6c762a11af5072bfda163b5ff95f4
AntonKopiika <toxxa099@gmail.com>
2021-05-18 15:34:57 +0300

    change localhost to ip address

--- config.py
+++ config.py
@@ -7,10 +7,7 @@ BASE_DIR = pathlib.Path(__file__).parent
 
 
 class Config:
-    """
-    'sqlite:///' + str(BASE_DIR / "data" / "db.sqlite3")
-    """
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://anton:Password_1234@localhost/department_project'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://anton:Password_1234@192.168.0.101/department_project'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = "Nothing"
     EMAIL_ADDRES = "antonkopiika24@gmail.com"
