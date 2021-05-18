8cde8d5ff0877ecfb4e8b42195bc3dab161f3ff6
AntonKopiika <toxxa099@gmail.com>
2021-05-18 16:26:15 +0300

    change localhost to host address

--- config.py
+++ config.py
@@ -7,7 +7,10 @@ BASE_DIR = pathlib.Path(__file__).parent
 
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://anton:Password_1234@192.168.0.101/department_project'
+    """
+    'sqlite:///' + str(BASE_DIR / "data" / "db.sqlite3")
+    """
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://testuser:Password_1234@192.168.0.101/department_project'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = "Nothing"
     EMAIL_ADDRES = "antonkopiika24@gmail.com"
